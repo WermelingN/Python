59bacae60fb7b2986adf77fb03bff7554d7ef068
Nick Wermeling <94858147+WermelingN@users.noreply.github.com>
2022-01-07 16:26:46 -0800

    Add files via upload

---
+++ numbers1-10.py
@@ -0,0 +1,15 @@
+i = 1
+
+while i <= 10:
+	print(i)
+	
+	i = i + 1
+
+if i % 2 == 0:
+	print("i is an even number")
+else:
+	print("i is an odd number")
+	
+for i in range(1):
+	print("this is a range function")
+	
